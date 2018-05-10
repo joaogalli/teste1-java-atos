3e2508ad137209625a4f627919c68495ecb5dd0b
João Eduardo Galli <joaogalli@gmail.com>
2018-05-10 17:10:59 -0300

    Criando filtro de skills na tela principal.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  skillsForm: FormGroup;
+  skillsOnFilter = [];
+
+  ngOnInit(): void {
+    this.skillsForm = new FormGroup({
+      'skill': new FormControl(null, [Validators.required] )
+    });
+  }
+
+  onAddSkill() {
+    let skill = this.skillsForm.value.skill;
+
+    let exists = this.skillsOnFilter.some(skillExists(skill));
+
+    if (skill && !exists) {
+      this.skillsOnFilter.push(skill);
+    }
+  }
+
+  removeSkill(skill) {
+    let indexOf = this.skillsOnFilter.indexOf(skill);
+    this.skillsOnFilter.splice(indexOf, 1);
+  }
+
+
+}
+
+function skillExists(skill) {
+  return function skillAlreadyExist(element, index, array) {
+    return (element === skill);
+  }
 }
